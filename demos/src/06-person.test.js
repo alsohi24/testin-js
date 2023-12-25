e5209026bb01c9386a394acac8519ba77769a549
Alvaro <alsohi24@gmail.com>
2023-12-25 08:31:12 -0500

    // AAA

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -1,14 +1,22 @@
+// AAA
+// Arrange / Given
+// Act / When
+// Assert / Then
 const Person = require('./06-person');
 
 describe('Test for Person', () => {
   let person;
+  // Arrange
   beforeEach(() => {
     person = new Person('Nicolas', 45, 1.7);
   });
 
   test('Should return down', () => {
+    // Arrange
     person.weight = 45;
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('down');
   });
 
